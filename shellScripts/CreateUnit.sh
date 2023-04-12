621d99efb2820ceef92fa704630261039e5bb10f
YYTWINT <yytwint.sisw@siemens.com>
2023-04-12 18:40:46 +0530

    Temp change to support CP build for container workflow

--- shellScripts/CreateUnit.sh
+++ shellScripts/CreateUnit.sh
@@ -11,3 +11,9 @@ fi
 NX_RELEASE=$1
 UNIT_PATH=$2
 /usr/site/devop_tools/bin/unit add -b -p @${NX_RELEASE} -t DEV -w SUB ${UNIT_PATH} -R y -O y -DO_LINK_OPT y
+
+initFile=${UNIT_PATH}/init.def
+sed -i 's/DO_TARI_RECOMPILES.*/DO_TARI_RECOMPILES       1/g' $initFile
+sed -i 's/DO_SOURCE_RECOMPILES.*/DO_SOURCE_RECOMPILES       1/g' $initFile
+sed -i 's/DO_LINK_OPT.*/DO_LINK_OPT       1/g' $initFile
+sed -i 's/DO_DEBUG.*/DO_DEBUG       0/g' $initFile
