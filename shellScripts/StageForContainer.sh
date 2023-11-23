a2db6781eb1f3fa0fb1bf171a5093fe758314e29
YYTWINT <yytwint.sisw@siemens.com>
2023-11-23 16:07:21 +0530

    Update StageForContainer.sh

--- shellScripts/StageForContainer.sh
+++ shellScripts/StageForContainer.sh
@@ -33,9 +33,11 @@ CONFIG_FILE_MULTICAD=${STAGE_DIR}/tessUG_multicad.config
 CONFIG_FILE_VIS=${STAGE_DIR}/tessUG_vis.config
 RUN_UGTOPV_MULTICAD=${STAGE_DIR}/run_ugtopv_multicad
 RUN_UGTOPV_VIS=${STAGE_DIR}/run_ugtopv_vis
+RUN_NXTOJT=${STAGE_DIR}/run_nxtojt
 
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/run_ugtopv_multicad ${RUN_UGTOPV_MULTICAD} || { exit 1;}
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/run_ugtopv_vis ${RUN_UGTOPV_VIS} || { exit 1;}
+cp -f ${CUSTOMER_ARTIFACTS_DIR}/run_nxtojt ${RUN_NXTOJT} || { exit 1;}
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/tessUG_multicad.config ${CONFIG_FILE_MULTICAD} || { exit 1;}
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/tessUG_vis.config ${CONFIG_FILE_VIS} || { exit 1;}
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/NXJT_Translator_README.txt ${STAGE_BASE_DIR}/ || { exit 1;}
@@ -44,6 +46,7 @@ chmod 0755 ${CONFIG_FILE_MULTICAD} || { exit 1;}
 chmod 0755 ${CONFIG_FILE_VIS} || { exit 1;}
 chmod 0755 ${RUN_UGTOPV_MULTICAD} || { exit 1;}
 chmod 0755 ${RUN_UGTOPV_VIS} || { exit 1;}
+chmod 0755 ${RUN_NXTOJT} || { exit 1;}
 
 # IP 10.146.116.128 is a flex licenser server IP
 # echo "ENV SPLM_LICENSE_SERVER=29000@10.146.116.128" >> ${STAGE_DIR}/dockerfile 
@@ -61,6 +64,7 @@ echo " " >> ${STAGE_DIR}/dockerfile
 echo "COPY run_ugtopv_vis         /app/run_ugtopv_vis" >> ${STAGE_DIR}/dockerfile
 echo "COPY tessUG_vis.config      /app/tessUG_vis.config" >> ${STAGE_DIR}/dockerfile
 echo "COPY run_ugtopv_multicad         /app/run_ugtopv_multicad" >> ${STAGE_DIR}/dockerfile
+echo "COPY run_nxtojt         /app/run_nxtojt" >> ${STAGE_DIR}/dockerfile
 echo "COPY tessUG_multicad.config      /app/tessUG_multicad.config" >> ${STAGE_DIR}/dockerfile
 echo " " >> ${STAGE_DIR}/dockerfile
 echo " " >> ${STAGE_DIR}/dockerfile
