a113d12895e2a9cba6bdea4867d52ea3451dfed5
YYTWINT <yytwint.sisw@siemens.com>
2025-02-17 12:36:51 +0530

    uncommented autotest

--- shellScripts/TestUnit.sh
+++ shellScripts/TestUnit.sh
@@ -10,20 +10,20 @@ fi
 
 UNIT_PATH=$1
 export SPLM_LICENSE_SERVER=29000@pnnxflex3:29000@pnnxflex4 
-# /usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6
-# casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
-# if [ $casesFailed != 0 ]
-# then
-	# /usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runfails -p 6 -keep -local -parent
-	# casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
-	# if [ $casesFailed != 0 ]
-	# then
-		# echo "Number of auto test cases failed for NXJT= $casesFailed...Exiting with error."
-		# exit 1
-	# fi
-# else
-	# echo "No test case failed. So skipping re-run of devtest."
-# fi
+/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6
+casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
+if [ $casesFailed != 0 ]
+then
+	/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runfails -p 6 -keep -local -parent
+	casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
+	if [ $casesFailed != 0 ]
+	then
+		echo "Number of auto test cases failed for NXJT= $casesFailed...Exiting with error."
+		exit 1
+	fi
+else
+	echo "No test case failed. So skipping re-run of devtest."
+fi
 
 mv "${UNIT_PATH}/dt" "${UNIT_PATH}/dt_nxjt"
 mv "${UNIT_PATH}/athena" "${UNIT_PATH}/athena_nxjt"
